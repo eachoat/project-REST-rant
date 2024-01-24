63dd0d707471fe62adfd2d80bff8ca59b7efe1f9
eachoat <eachoat@gmail.com>
2024-01-23 21:11:07 -0500

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// Express Settings
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
+// Controllers & Routes
 app.use('/places', require('./controllers/places'))
 
-app.get ('/', (req, res) => {
+app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get ('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
-
+app.get('*', (req, res) => {
+    res.render('error404')
 })
 
-app.listen (process.env.PORT)
+// Listen for Connections
+app.listen(process.env.PORT)
+
