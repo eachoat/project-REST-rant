e360c9816c2081c44028b8c3ba0e9d5afa59a278
eachoat <eachoat@gmail.com>
2024-01-23 21:57:45 -0500

    Adding Bootstrap grid to index page

--- views/error404.jsx
+++ views/error404.jsx
@@ -6,6 +6,13 @@ function error404 () {
       <Def>
           <main>
               <h1>404: PAGE NOT FOUND</h1>
+                <div>
+                  <img src="/images/pizza-pic.jpg" alt="donkey picture" />
+                  <div>
+                  Photo by <a href="https://unsplash.com/@dan_scape?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Dan Cook</a> on <a href="https://unsplash.com/photos/long-coated-brown-animal-MCauAnBJeig?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+                  </div>
+                </div>
               <p>Oops, sorry, we can't find this page!</p>
           </main>
       </Def>
--- views/home.jsx
+++ views/home.jsx
@@ -6,10 +6,23 @@ function home () {
       <Def>
           <main>
               <h1>HOME</h1>
+              <div>
+                <img src="/images/pizza-pic.jpg" alt="pizza picture" />
+                <div>
+                  Photo by <a href="AUTHOR_LINK">Chad Montano</a> on <a href="https://unsplash.com/photos/pizza-on-chopping-board-MqT0asuoIcU">Unsplash</a>
+                </div>
+              </div>
+              <a href="/places">
+                  <button className="btn-primary">Places Page</button>
+              </a>
+
           </main>
+
+         
       </Def>
     )
   }
+
   
 
 module.exports = home
