451ca0a8fdca54c1de7b7765183dd7515baf8e5a
eachoat <eachoat@gmail.com>
2024-02-01 21:31:34 -0500

    2/1 Updates

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,39 +5,46 @@ function show (data) {
     return (
         <Def>
           <main>
-            <h1>{data.place.name}</h1>
+                <div className="row">
+                    <h1>{data.place.name}</h1>
 
-            <section class="col-md-6 text-right">
-                <h2 className="body" >Ratings</h2>
-                    <div>currently unrated</div>
-            </section>
+                    <section class="col-sm-6">
+                        <h2 className="body" >Ratings</h2>
+                            <div>currently unrated</div>
 
-            <section>
-                    <img class="img-fluid">{data.place.pic}</img>
-            </section>
+                        <img class="img-fluid">{data.place.pic}</img>
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </section>
 
-            <section class="col-md-6 text-right">
-                <h2 className="body" >Description</h2>
-                    <div>Located in {data.place.city},{data.place.state} and serving {data.place.cuisines}</div>
-            </section>
+                    <section class="col-sm-6">
+                        <h2 className="body" >Description</h2>
+                        <h3>
+                            {data.place.showEstablished()}
+                        </h3>
+                        <h4>
+                            Serving {data.place.cuisines}
+                        </h4>
+                    </section>
 
-            <section class="col-md-6 text-right">
-                <h2 className="body" >Comments</h2>
-                    <div>No comments yet!</div>
-            </section>
+                    <section class="col-sm-6">
+                        <h2 className="body" >Comments</h2>
+                            <div>No comments yet!</div>
+                    </section>
 
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-            </a>     
-
-
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">
-                    Delete
-                </button>
-            </form>     
+                    <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
+                        Edit
+                    </a>     
 
 
+                    <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+                        <button type="submit" className="btn btn-danger">
+                            Delete
+                        </button>
+                    </form>     
+
+                </div>
           </main>
         </Def>
     )
